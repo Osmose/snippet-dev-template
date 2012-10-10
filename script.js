91f89258012abfad6abd1492a266df35d335b567
Michael Kelly <mkelly@mozilla.com>
2012-10-10 11:42:45 -0400

    Remove flicks-video specific stuff.

--- script.js
+++ script.js
@@ -1,23 +1,10 @@
 (function() {
-    var snippet = document.getElementById('flicks-video-snippet');
-    var message = document.getElementById('flicks-video-message');
-    var video = document.getElementById('flicks-video');
-    var play_link = document.getElementById('flicks-video-play');
-
-    // Display and play the video after the message has faded.
-    message.addEventListener('transitionend', function(e) {
-        message.style.display = 'none';
-        video.className = 'show';
-
-        video.play();
-    }, false);
-
-    // Bind click event only after snippet has loaded.
+    // Uncomment and customize the code below to run a function when your
+    // specific snippet is shown to the user.
+    /*
+    var snippet = document.getElementById('snippet-id')
     snippet.addEventListener('show_snippet', function() {
-        // Trigger animation when play link is clicked.
-        play_link.addEventListener('click', function(e) {
-            message.className = 'hidden';
-            e.stopPropagation();
-        }, false);
+        // Insert on-show code here.
     }, false);
+    */
 })();
